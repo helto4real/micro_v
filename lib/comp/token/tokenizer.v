f91523a75e88dfe8094cc5590a79c471d9185750
helto4real <tomas.hellstrom@yahoo.se>
2021-02-11 20:38:32 +0100

    More stuff and tests

--- lib/comp/token/tokenizer.v
+++ lib/comp/token/tokenizer.v
@@ -1,11 +1,16 @@
 module token
 
+const (
+	single_quote = `\'`
+	double_quote = `"`
+)
+
 pub struct Tokenizer {
 	code string // code tokenized
 mut:
-	pos    int 		   // current position in file
-	ln     int = 1 	   // current line being parsed
-	col    int = 1 	   // current colum being parsed
+	pos    int  // current position in file
+	ln     int  = 1 // current line being parsed
+	col    int  = 1 // current colum being parsed
 	ch     byte = `\0` // current char
 	is_eof bool
 }
@@ -13,12 +18,19 @@ mut:
 // instance a tokenizer from string
 pub fn new_tokenizer_from_string(code string) &Tokenizer {
 	if code.len > 0 {
-
 	}
 	return &Tokenizer{
 		code: code
-		ch: if code.len > 0 {code[0]} else {`\0`}
-		is_eof: if code.len > 0 {false} else {true}
+		ch: if code.len > 0 {
+			code[0]
+		} else {
+			`\0`
+		}
+		is_eof: if code.len > 0 {
+			false
+		} else {
+			true
+		}
 	}
 }
 
@@ -26,21 +38,83 @@ pub fn new_tokenizer_from_string(code string) &Tokenizer {
 pub fn (mut t Tokenizer) next_token() Token {
 	// whitepace has no meaning and will not be parsed
 	t.skip_whitespace()
+	
 	if t.is_eof {
 		return t.token(.eof, 'eof', 1)
 	}
 	nextc := t.peek(1)
-	
+
+	// Check for identifiers and keyword identifiers
 	if is_name_char(t.ch) {
-			name := t.ident_name()
-			kind := keywords[name]
-			if kind == .unknown {
-				return t.token(.name, name, name.len)
+		name := t.name_tok()
+		kind := keywords[name]
+		if kind == .unknown {
+			return t.token(.name, name, name.len)
+		} else {
+			return t.token(kind, name, name.len)
+		}
+	} else if t.ch.is_digit() {
+		number := t.number()
+		return t.token(.number, number, number.len)
+	} 
+	match t.ch {
+		`(` {
+			t.token(.lpar, '(', 1)
+		}
+		`)` {
+			t.token(.rpar, ')', 1)
+		}
+		`{` {
+			t.token(.lcbr, '{', 1)
+		}
+		`}` {
+			t.token(.rcbr, '}', 1)
+		}
+		single_quote, double_quote {
+			ident_string := t.string_tok()
+			return t.token(.string, ident_string, ident_string.len)
+		}
+		`:` {
+			if nextc == `=` {
+				return t.token(.decl_assign, ':=', 2)
 			} else {
-				return t.token(kind, name, name.len)
+				return t.token(.colon, ':', 1)
 			}
-		} 
-	return t.token(.error, '${t.ch.ascii_str()}', 1)
+		}
+		else {return t.token(.error, '$t.ch.ascii_str()', 1)}
+	}
+		// `=` {
+		// 	if nextc == `=` {
+		// 		s.pos++
+		// 		return s.token_unknown() 
+		// 	} else {
+		// 		return s.token_assign()
+		// 	}
+
+		// }
+		// `.` {
+		// 	return s.token_dot()
+		// }
+		// `/` {
+		// 	if nextc == `/` {
+		// 		// Line comment
+		// 		s.skip_comment(true)
+		// 	} else if nextc == `*` {
+		// 		// Normal comment
+		// 		s.skip_comment(false)
+		// 	} else {
+		// 		s.token_div()
+		// 	}
+		// }
+		// `&` {					
+		// 	return s.token_amp()
+		// }
+		// else {
+		// 	s.pos++
+		// 	return s.token_unknown()
+		// }
+
+	return t.token(.error, '$t.ch.ascii_str()', 1)
 }
 
 // token instance new token of a kind
@@ -59,7 +133,6 @@ fn (mut t Tokenizer) token(kind Kind, lit string, len int) Token {
 	return tok
 }
 
-[inline]
 // next, get next char
 fn (mut t Tokenizer) next() {
 	if t.is_eof {
@@ -68,30 +141,28 @@ fn (mut t Tokenizer) next() {
 	t.pos++
 	t.col++
 	if t.pos >= t.code.len {
-		t.is_eof = true 
+		t.is_eof = true
 		t.ch = `\0`
 		return
 	}
 	t.ch = t.code[t.pos]
-	
 }
 
-[inline]
 // skip, skips n chars
 fn (mut t Tokenizer) skip(n int) {
 	if t.pos + n < t.code.len {
-		t.pos+=n
-		t.col+=n
+		t.pos += n
+		t.col += n
+		t.ch = t.code[t.pos]
 	} else {
-		t.col+=n
-		t.pos = t.code.len 
+		t.col += n
+		t.pos = t.code.len
 		t.is_eof = true
 		t.ch = `\0`
 		// todo: error here
 	}
 }
 
-[inline]
 // peek, peeks the character at pos + n or '\0' if eof
 fn (mut t Tokenizer) peek(n int) byte {
 	if t.pos + n < t.code.len {
@@ -101,13 +172,11 @@ fn (mut t Tokenizer) peek(n int) byte {
 	}
 }
 
-
-[inline]
 // skip_whitespace, skips all whitespace characters
 fn (mut t Tokenizer) skip_whitespace() {
 	for !t.is_eof && t.ch.is_space() {
 		if t.ch == `\r` {
-			//Count \r\n as one line
+			// Count \r\n as one line
 			if t.peek(1) == `\n` {
 				t.next()
 				t.inc_line_nr()
@@ -119,36 +188,66 @@ fn (mut t Tokenizer) skip_whitespace() {
 	}
 }
 
-[inline]
 // inc_line_nr, increments line number
 fn (mut t Tokenizer) inc_line_nr() {
 	t.ln++
 	t.col = 1
 }
 
-[inline]
 // is_name_char returns true if character is in a name
 pub fn is_name_char(c byte) bool {
 	return (c >= `a` && c <= `z`) || (c >= `A` && c <= `Z`) || c == `_`
 }
 
-[inline]
-// ident_name, gets the name of identifier
-fn (mut t Tokenizer) ident_name() string {
+// name, gets the name token
+fn (mut t Tokenizer) name_tok() string {
 	start := t.pos
-	t.next()
-	for !t.is_eof && (is_name_char(t.ch) || t.ch.is_digit()) {
-		t.next()
+	mut len := 1
+	mut peek := t.peek(len)
+	for peek != `\0` && (is_name_char(peek) || peek.is_digit()) {
+		peek = t.peek(len)
+		len++
 	}
-	name := t.code[start..t.pos]
-	// s.pos--
-	return name
+	return t.code[start..(start + len - 1)]
 }
 
-[inline]
+fn (mut t Tokenizer) string_tok() string {
+	start_pos := t.pos + 1
+	mut len := 1
+	mut peek := t.peek(len)
+	q_char := t.ch
+	for {
+		if peek == `\0` {
+			// Todo: fix error
+			// s.error('unfinished string literal')
+			break
+		}
+		if peek == q_char {
+			if len>1 {
+				return t.code[start_pos..start_pos+len-1]
+			} else {
+				return ''
+			}
+		}
+		len++
+		peek = t.peek(len)
+	}
+	return ''
+}
+
+// name, gets the number token
+fn (mut t Tokenizer) number() string {
+	start := t.pos
+	mut len := 1
+	mut peek := t.peek(len)
+	for peek != `\0` && peek.is_digit() {
+		peek = t.peek(len)
+		len++
+	}
+	return t.code[start..(start + len - 1)]
+}
+
 // is_nl returns true if character is new line
 pub fn is_nl(c byte) bool {
 	return c == `\r` || c == `\n`
 }
-
-
--- lib/comp/token/tokenizer_test.v
+++ lib/comp/token/tokenizer_test.v
@@ -1,10 +1,13 @@
 module token
 
+// EOF specific tests
 fn test_last_is_eof() {
 	mut tkz := new_tokenizer_from_string('')
 	tok := tkz.next_token()
 	assert tok.kind == .eof 
 }
+
+// name token tests
 
 fn test_name() {
 	mut tkz := new_tokenizer_from_string('hello')
@@ -12,5 +15,95 @@ fn test_name() {
 	assert tok.kind == .name 
 	assert tok.lit == 'hello' 
 	assert tok.pos.pos == 0
-	
 }
+
+fn test_two_names() {
+	mut tkz := new_tokenizer_from_string('hello world')
+	mut tok := tkz.next_token()
+	assert tok.kind == .name 
+	assert tok.lit == 'hello' 
+	assert tok.pos.pos == 0
+
+	tok = tkz.next_token()
+	assert tok.kind == .name 
+	assert tok.lit == 'world' 
+	assert tok.pos.pos == 6
+}
+
+// number token tests
+
+fn test_number() {
+	mut tkz := new_tokenizer_from_string('12345')
+	tok := tkz.next_token()
+	assert tok.kind == .number 
+	assert tok.lit == '12345' 
+	assert tok.pos.pos == 0
+}
+
+// name and number compo tests
+fn test_name_and_number() {
+	mut tkz := new_tokenizer_from_string('hello 12345')
+	mut tok := tkz.next_token()
+	assert tok.kind == .name 
+	assert tok.lit == 'hello' 
+	assert tok.pos.pos == 0
+
+	tok = tkz.next_token()
+	assert tok.kind == .number 
+	assert tok.lit == '12345' 
+	assert tok.pos.pos == 6
+}
+
+// name and number compo tests
+fn test_number_and_name() {
+	mut tkz := new_tokenizer_from_string('12345 hello')
+	mut tok := tkz.next_token()
+	assert tok.kind == .number 
+	assert tok.lit == '12345' 	
+	assert tok.pos.pos == 0
+
+	tok = tkz.next_token()
+	assert tok.kind == .name 
+	assert tok.lit == 'hello' 
+	assert tok.pos.pos == 6
+}
+
+
+fn test_name_and_number_and_namewithnumber() {
+	mut tkz := new_tokenizer_from_string('hello 12345 namewith1234')
+	mut tok := tkz.next_token()
+	assert tok.kind == .name 
+	assert tok.lit == 'hello' 
+	assert tok.pos.pos == 0
+
+	tok = tkz.next_token()
+	assert tok.kind == .number 
+	assert tok.lit == '12345' 
+	assert tok.pos.pos == 6
+
+	tok = tkz.next_token()
+	assert tok.kind == .name 
+	assert tok.lit == 'namewith1234' 
+	assert tok.pos.pos == 12
+}
+
+// Test the string literal
+fn test_string_literal_double_quote() {
+	mut tkz := new_tokenizer_from_string('"hello world"')
+	mut tok := tkz.next_token()
+	assert tok.kind == .string
+	assert tok.lit == 'hello world' 
+}
+
+fn test_string_literal_single_quote() {
+	mut tkz := new_tokenizer_from_string("'hello world'")
+	mut tok := tkz.next_token()
+	assert tok.kind == .string
+	assert tok.lit == 'hello world' 
+}
+
+fn test_string_literal_error_missing_end_quote() {
+	mut tkz := new_tokenizer_from_string("'hello world")
+	mut tok := tkz.next_token()
+	assert tok.lit == 'hello world' 
+	assert tok.kind == .string
