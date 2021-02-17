68dfa4efb4c517f446c7aa79243e49c48cdf15db
helto4real <tomas.hellstrom@yahoo.se>
2021-02-17 20:30:02 +0100

    Text line number in position

--- lib/comp/parser/parser_test.v
+++ lib/comp/parser/parser_test.v
@@ -31,10 +31,72 @@ fn test_next_token() {
 
 fn test_match_token() {
 	mut p := new_parser_from_text('abc 123')
-	tok := p.match_token(.name)
+	mut tok := p.match_token(.name)
 	assert tok.kind == .name
 	assert tok.pos.pos == 0
+	assert tok.pos.len == 3
+	assert tok.pos.col == 1
+	assert tok.pos.ln == 1
 	assert tok.lit == 'abc'
+
+	tok = p.match_token(.number)
+	assert tok.kind == .number
+	assert tok.pos.pos == 4
+	assert tok.pos.len == 3
+	assert tok.pos.col == 5
+	assert tok.pos.ln == 1
+	assert tok.lit == '123'
+}
+
+fn test_match_line_token() {
+	mut p := new_parser_from_text('abc 123\ncba 321\r\nabc 123')
+	mut tok := p.match_token(.name)
+	assert tok.kind == .name
+	assert tok.pos.pos == 0
+	assert tok.pos.len == 3
+	assert tok.pos.col == 1
+	assert tok.pos.ln == 1
+	assert tok.lit == 'abc'
+
+	tok = p.match_token(.number)
+	assert tok.kind == .number
+	assert tok.pos.pos == 4
+	assert tok.pos.len == 3
+	assert tok.pos.col == 5
+	assert tok.pos.ln == 1
+	assert tok.lit == '123'
+
+	tok = p.match_token(.name)
+	assert tok.kind == .name
+	assert tok.pos.pos == 8
+	assert tok.pos.len == 3
+	assert tok.pos.col == 1
+	assert tok.pos.ln == 2
+	assert tok.lit == 'cba'
+
+	tok = p.match_token(.number)
+	assert tok.kind == .number
+	assert tok.pos.pos == 12
+	assert tok.pos.len == 3
+	assert tok.pos.col == 5
+	assert tok.pos.ln == 2
+	assert tok.lit == '321'
+
+	tok = p.match_token(.name)
+	assert tok.kind == .name
+	assert tok.pos.pos == 17
+	assert tok.pos.len == 3
+	assert tok.pos.col == 1
+	assert tok.pos.ln == 3
+	assert tok.lit == 'abc'
+
+	tok = p.match_token(.number)
+	assert tok.kind == .number
+	assert tok.pos.pos == 21
+	assert tok.pos.len == 3
+	assert tok.pos.col == 5
+	assert tok.pos.ln == 3
+	assert tok.lit == '123'
 }
 
 fn test_match_token_not_exixt() {
