e448706f0dd097545eda71c5399ea2e55a63f744
helto4real <tomas.hellstrom@yahoo.se>
2021-04-18 19:28:36 +0200

    fixed hello world

--- samples/hello/hello_world.v
+++ samples/hello/hello_world.v
@@ -1,17 +1,3 @@
-// fn main() {
-// 	println('hello world!')
-// }
-
-fn no_mut_or_ref(x int) {
-	assert x == 10
-}
-
-fn mutable_function_param_with_assert(mut x int) {
-	no_mut_or_ref(x)
-}
-
 fn main() {
-	mut x:= 100
-	mutable_function_param_with_assert(mut x)
-	assert x == 10
+	println('hello world!')
 }
